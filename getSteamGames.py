b5c4c122365cbe14dd456a12d82d2b2d7e7a7bad
GitKraken <alexander.oswald@web.de>
2021-01-24 11:43:25 +0100

    Change search from ID-based to Name-based. Add logging.

--- getSteamGames.py
+++ getSteamGames.py
@@ -1,4 +1,9 @@
-import re #, bs4, requests, sys
+#! python3
+
+import logging, re, bs4, requests#, sys
+logging.basicConfig(filename="steamGames.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s)")
+
+logging.debug("Start of programm")
 
 pfad = "steamHTML\\" # all steam-user HTML-Files should be placed in this directory
 dateiname1="SteamGames1.html"
@@ -10,16 +15,39 @@ datei2 = pfad+dateiname2
 datei3 = pfad+dateiname3
 
 # Create Games-Array by STEAM-User HTML File
-def getGames(PATH):
-
+def getGamesIDs(PATH):
+    
     in_file = open(PATH,"r",encoding="utf8")
     text = in_file.read()
-    # soup = bs4.BeautifulSoup(text, "html.parser")
+    soup = bs4.BeautifulSoup(text, "html.parser")
     in_file.close()
         
     games = ([])
     games = re.findall("game_\d\d\d+", text)
+    logging.debug("Start: "+PATH)
+    logging.debug(games)
+    logging.info(PATH+" Number of games from IDs: "+str(len(games)))
+    logging.debug("Finished "+PATH)
     return games
+
+# Create Games-Array by STEAM-User HTML File
+def getGamesNames(PATH):
+    
+    in_file = open(PATH,"r",encoding="utf8")
+    text = in_file.read()
+    soup = bs4.BeautifulSoup(text, "html.parser")
+    in_file.close()
+        
+    games = ([])
+    games = re.findall("""Name ellipsis ">.+""", text) # Pattern contains 14 chars to be in line with following patterns 
+    games = games + re.findall("""or_uninstalled">.+""", text) # own games list contains also tags: ellipsis color_uninstalled"
+    games = games + re.findall("""color_disabled">.+""", text) # own games list contains also tags: ellipsis color_disabled"
+    logging.debug("Start: "+PATH)
+    logging.debug(games)
+    logging.info(PATH+" Number of games from Names: "+str(len(games)))
+    logging.debug("Finished "+PATH)
+    return games
+
 
 # Compare two game-sets and create a set of matching games
 def getCommon(games1, games2):
@@ -29,6 +57,7 @@ def getCommon(games1, games2):
             if (game2 == game1):
                 #print ("append: "+game1)
                 common_games.append(game1)
+    #print('common ended')
     return common_games
 
 # extract IDs from "game_ID"-String 
@@ -36,8 +65,16 @@ def getIDs(games):
     gameIDs=([])
     for game in games:
         gameIDs.append(game[5:])
-    #print (game)
+    #print (game)sis color_disabled
     return gameIDs
+
+# strip Name
+def getNames(games):
+    gameNames=([])
+    for game in games:
+        gameNames.append(game[16:-6])
+    #print (game)
+    return gameNames
 
 # create link to SteamStore from GameID
 def gameLink(NUMBER):
@@ -48,14 +85,24 @@ common_games1 = ([])
 common_games2= ([])
 
 # Read game lists for three players
-games1 = getGames(datei1)
-games2 = getGames(datei2)
-games3 = getGames(datei3)
+gamesNames1 = getNames(getGamesNames(datei1))
+gamesIDs1 = getGamesIDs(datei1)
+gamesNames2 = getNames(getGamesNames(datei2))
+gamesIDs2 = getGamesIDs(datei2)
+gamesNames3 = getNames(getGamesNames(datei3))
+gamesIDs3 = getGamesIDs(datei3)
 
 # Find matches
-common_games1 = getCommon(games1, games2)
+common_games1 = getCommon(gamesNames1, gamesNames3)
+common_games1ID = getCommon(gamesNames1, gamesNames2)
 #print (common_games1)
-common_games2 = getCommon(games3,common_games1)
+common_games2 = getCommon(gamesNames2,common_games1)
 
-print (getIDs(common_games2))
+# print ("found games1: ")
+# print ((common_games1))
+logging.info("found common games: " +str(len((common_games2))))
+print ("found common games: " +str(len((common_games2))))
+print ((common_games2))
+#print ("found: "+str(len(common_games2))+"games: "+getNames(common_games2))
 
+logging.debug("End of program")
